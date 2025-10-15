159b17daf744f433a6a34b04b82c52c774ef5ba3
rohan694 <36160002+rohan694@users.noreply.github.com>
2025-10-15 23:31:04 +0530

    Add imports for Arrays and List in SecurityConfig

--- src/main/java/com/logicleaf/invplatform/config/SecurityConfig.java
+++ src/main/java/com/logicleaf/invplatform/config/SecurityConfig.java
@@ -18,6 +18,10 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfigurationSource;
 
+import java.util.Arrays;
+import java.util.List;
+
+
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(
